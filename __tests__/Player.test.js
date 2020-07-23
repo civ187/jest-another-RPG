acdbe767d7ac73ff22728be3b60adec3b0dea6d1
Victor Cesar Lopez <civ187@gmail.com>
2020-07-22 22:28:28 -0500

    10.3.4 7/22/2020 @ 10:238 pm

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,7 +4,7 @@ const Potion = require('../lib/Potion');
 //jest.mock() mocks/replaces the constructor's implementation with our faked data.
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+// console.log(new Potion());
 
 const Player = require('../lib/Player');
 
@@ -69,3 +69,30 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()]; //Creates array of potion's
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
